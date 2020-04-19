d0b12210441bd7d94d5684369b62fb67ddac968b
shekharsr7 <61740313+shekharsr7@users.noreply.github.com>
2020-04-19 19:15:48 +0530

    visitor functionality (#12)
    Co-authored-by: Shekhar R <a-8177@LE1016.local>

---
+++ src/main/java/com/codepirates/securitydesk/controller/VisitorController.java
@@ -0,0 +1,46 @@
+package com.codepirates.securitydesk.controller;
+
+import com.codepirates.securitydesk.model.Visitor;
+import com.codepirates.securitydesk.repository.VisitorDAL;
+import com.codepirates.securitydesk.repository.VisitorRepository;
+import com.codepirates.securitydesk.util.CommonFunctions;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class VisitorController {
+
+    private final VisitorRepository visitorRepository;
+    private final VisitorDAL visitorDAL;
+    private final CommonFunctions commonFunctions;
+
+    public VisitorController(VisitorRepository visitorRepository, VisitorDAL visitorDAL, CommonFunctions commonFunctions) {
+        this.visitorRepository = visitorRepository;
+        this.visitorDAL = visitorDAL;
+        this.commonFunctions = commonFunctions;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/addVisitor")
+    public List<Visitor> saveVisitor(@RequestBody Visitor visitor) {
+        visitor.setCheckInTime(commonFunctions.currentDateAndTime());
+        visitorDAL.addVisitor(visitor);
+        return getAllVisitor();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/updateVisitor/{tagId}")
+    public List<Visitor> updateVisitor(@PathVariable String tagId) {
+        visitorDAL.updateVisitor(tagId);
+        return getAllVisitor();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/getAllVisitor")
+    public List<Visitor> getAllVisitor() {
+        return visitorRepository.findAllByCheckOutTimeIsNotNull();
+    }
+
+}
---
+++ src/main/java/com/codepirates/securitydesk/repository/VisitorDALImpl.java
@@ -0,0 +1,35 @@
+package com.codepirates.securitydesk.repository;
+
+import com.codepirates.securitydesk.model.Visitor;
+import com.codepirates.securitydesk.util.CommonFunctions;
+import com.mongodb.client.result.UpdateResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class VisitorDALImpl implements VisitorDAL {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Autowired
+    CommonFunctions commonFunctions;
+
+    @Override
+    public Visitor addVisitor(Visitor visitor){
+        return mongoTemplate.save(visitor);
+    }
+
+    @Override
+    public UpdateResult updateVisitor(String tagId){
+        Query query = new Query();
+        query.addCriteria (Criteria.where ("tagId").is (tagId));
+        return mongoTemplate.updateFirst(query, Update.update("checkOutTime", commonFunctions.currentDateAndTime()), Visitor.class);
+    }
+}
